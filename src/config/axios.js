da19bd993cff23d5ee8dbdef628efbe475d2c140
BaGreal2 <artemonik17@gmail.com>
2022-11-02 18:47:03 +0200

    axios change

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,4 +1,2 @@
 import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:5001";
-//   `${process.env.REACT_APP_API_URL}` || "http://localhost:5001";
+axios.defaults.baseURL = "https://wireworld-server.herokuapp.com";
