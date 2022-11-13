7e82193d1398178aee11782ef49332daf9be4b01
BaGreal2 <artemonik17@gmail.com>
2022-11-13 23:08:12 +0200

    Changed server to cyclic

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
 // axios.defaults.baseURL = 'https://wireworld-server.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:5001';
+// axios.defaults.baseURL = 'http://localhost:5001';
+axios.defaults.baseURL = 'https://talented-cow-nightgown.cyclic.app';
