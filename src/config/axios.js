0f80efdb0b847fac2f55095a7eced1ffd74d9ea3
BaGreal2 <artemonik17@gmail.com>
2022-11-23 16:13:35 +0200

    link changed axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5001';
-// axios.defaults.baseURL = 'https://talented-cow-nightgown.cyclic.app';
+// axios.defaults.baseURL = 'http://localhost:5001';
+axios.defaults.baseURL = 'https://talented-cow-nightgown.cyclic.app';
