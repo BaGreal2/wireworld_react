d966d03e472db4801ef770018a0b52a9c120692c
BaGreal2 <artemonik17@gmail.com>
2022-12-06 23:00:22 +0200

    axios link update

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
 //axios.defaults.baseURL = 'http://localhost:5001';
-axios.defaults.baseURL = 'https://talented-cow-nightgown.cyclic.app';
+// axios.defaults.baseURL = 'https://talented-cow-nightgown.cyclic.app';
+axios.defaults.baseURL = 'https://wireworld-server-app.onrender.com';
