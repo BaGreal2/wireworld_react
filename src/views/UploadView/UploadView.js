70cd3f265402ab0292adb2641b6b8d3bc043603c
BaGreal2 <artemonik17@gmail.com>
2022-12-08 14:44:05 +0200

    mobile alert + burger fixed

--- src/views/UploadView/UploadView.js
+++ src/views/UploadView/UploadView.js
@@ -12,91 +12,91 @@ import { setLanguage, setTheme, updateDict } from '../../handlers/lookSwitches';
 import styles from './styles/UploadView.module.css';
 
 export default function UploadView() {
-	const [lang, setLangState] = useState(localStorage.getItem('language'));
-	const [theme, setThemeState] = useState(localStorage.getItem('theme'));
-	const [dict, setDict] = useState(dictionary.eng);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(null);
-	let navigate = useNavigate();
+  const [lang, setLangState] = useState(localStorage.getItem('language'));
+  const [theme, setThemeState] = useState(localStorage.getItem('theme'));
+  const [dict, setDict] = useState(dictionary.eng);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  let navigate = useNavigate();
 
-	useEffect(() => {
-		updateDict(lang, dict, setDict);
-	}, [lang, theme, dict]);
+  useEffect(() => {
+    updateDict(lang, dict, setDict);
+  }, [lang, theme, dict]);
 
-	function createSchema(data) {
-		data.preventDefault();
-		setLoading(true);
-		let title = data.target[0].value;
-		let description = data.target[1].value;
-		data = {
-			title,
-			description,
-			cell_arr: localStorage.getItem('grid'),
-			size: JSON.parse(localStorage.getItem('size')),
-			rating: [],
-			creator: JSON.parse(JSON.parse(localStorage.getItem('persist:auth')).user)
-				._id,
-			creatorName: JSON.parse(
-				JSON.parse(localStorage.getItem('persist:auth')).user
-			).username,
-		};
+  function createSchema(data) {
+    data.preventDefault();
+    setLoading(true);
+    let title = data.target[0].value;
+    let description = data.target[1].value;
+    data = {
+      title,
+      description,
+      cell_arr: localStorage.getItem('grid'),
+      size: JSON.parse(localStorage.getItem('size')),
+      rating: [],
+      creator: JSON.parse(JSON.parse(localStorage.getItem('persist:auth')).user)
+        ._id,
+      creatorName: JSON.parse(
+        JSON.parse(localStorage.getItem('persist:auth')).user
+      ).username,
+    };
 
-		axios({
-			method: 'POST',
-			url: '/schemas',
-			data,
-		})
-			.then((res) => {
-				navigate(`/schemas/${res.data._id}`, true);
-			})
-			.catch((error) => {
-				console.dir(error);
-				setError(error);
-				setLoading(false);
-			})
-			.finally(() => setLoading(false));
-	}
+    axios({
+      method: 'POST',
+      url: '/schemas',
+      data,
+    })
+      .then((res) => {
+        navigate(`/schemas/${res.data._id}`, true);
+      })
+      .catch((error) => {
+        console.dir(error);
+        setError(error);
+        setLoading(false);
+      })
+      .finally(() => setLoading(false));
+  }
 
-	const toggleLang = () => {
-		setLanguage(setLangState);
-	};
+  const toggleLang = () => {
+    setLanguage(setLangState);
+  };
 
-	const toggleTheme = () => {
-		setTheme(setThemeState);
-	};
+  const toggleTheme = () => {
+    setTheme(setThemeState);
+  };
 
-	return (
-		<Container>
-			<Topbar
-				theme_func={toggleTheme}
-				lang_func={toggleLang}
-				needLang={true}
-				needBack={true}
-				lang={lang}
-			></Topbar>
-			{error && !loading && (
-				<Alert
-					alertTitle={error.message}
-					alertDescription={error.responce?.data.message}
-					className="upload-alert"
-				/>
-			)}
-			{loading && (
-				<div className={styles.circular_container}>
-					<CircularProgress
-						isIndeterminate
-						size="100px"
-						thickness="5px"
-						color="black"
-					/>
-				</div>
-			)}
-			<UploadForm
-				title={dict.title}
-				description={dict.description}
-				post={dict.post}
-				onSubmit={createSchema}
-			/>
-		</Container>
-	);
+  return (
+    <Container>
+      <Topbar
+        theme_func={toggleTheme}
+        lang_func={toggleLang}
+        needLang={true}
+        needBack={true}
+        lang={lang}
+      ></Topbar>
+      {error && !loading && (
+        <Alert
+          alertTitle={error.message}
+          alertDescription={error.responce?.data.message}
+          className="upload-alert"
+        />
+      )}
+      {loading && (
+        <div className={styles.circular_container}>
+          <CircularProgress
+            isIndeterminate
+            size="100px"
+            thickness="5px"
+            color="black"
+          />
+        </div>
+      )}
+      <UploadForm
+        title={dict.title}
+        description={dict.description}
+        post={dict.post}
+        onSubmit={createSchema}
+      />
+    </Container>
+  );
 }
